9ac873308290f166818e0f03db52a632bebc8c83
malakmalak2005123-cmd <malakmalak2005123@gmail.com>
2026-01-08 12:38:51 +0100

    update

--- app/configue.php
+++ app/configue.php
@@ -1,17 +1,10 @@
 <?php
-phpinfo();
 // Use Railway environment variables
 $host =$ip = getenv('MYSQLHOST', true) ?: getenv('MYSQLHOST');
 $port = getenv('MYSQLPORT', true) ?: getenv('MYSQLPORT');       // port number
 $db   = getenv('MYSQL_DATABASE', true) ?: getenv('MYSQL_DATABASE');   // database name
 $user = getenv('MYSQLUSER', true) ?: getenv('MYSQLUSER');       // username
 $pass = getenv('MYSQLPASSWORD', true) ?: getenv('MYSQLPASSWORD');   // password
-
-echo 'host: '.$host;
-echo 'port: '.$port;
-echo 'db: '.$db;
-echo 'user: '.$user;
-echo 'pass: '.$pass;
 
 try {
     $pdo = new PDO(
